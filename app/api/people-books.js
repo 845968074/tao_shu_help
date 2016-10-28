339f9dcc43c6f31089a4dc39c0e6c967f79f05a2
845968074 <845968074@qq.com>
2016-10-28 16:10:39 +0800

    tao bao book

---
+++ app/api/people-books.js
@@ -0,0 +1,42 @@
+'use strict';
+import express from 'express';
+import {Book} from '../mongodb/schema';
+import _ from 'lodash';
+const router = express.Router();
+router.post('/', function (req, res, next) {
+
+    let people_books = [{
+        author: '',
+        book_name: '',
+        press: '',
+        images: '',
+        count: 0,
+        price: 0,
+        states: ''
+    }], i = 0;
+    let books = [];
+  /*  Book.remove({
+
+    }, function (e) {
+        if (e) res.send(e.message);
+        else console.log("删除成功");
+    });*/
+    Book.find({}, function (err, people_book) {
+        console.log("all book: "+people_book);
+        if (err) return next(err);
+        _.map(people_book, function ({
+            author, name, press, images, count, price, state, _id
+        }) {
+            if (state === true)
+                people_books[i++] = ({author, name, press, images, count, price, state, _id});
+        });
+        for (let j = 0; j <people_books.length; j++) {
+            books.push(people_books[j]);
+        }
+        return res.status(201).json({people_books: books});
+    })
+
+
+});
+
+export default router;
