d38970f1a200f7a67c1abd44bf16266022b84cba
845968074 <845968074@qq.com>
2016-10-28 17:54:16 +0800

    book

--- app/mongodb/db.js
+++ app/mongodb/db.js
@@ -1,7 +1,6 @@
 import mongoose from'mongoose';
 module.exports = {
     connect: function (mode, callback) {
-        //let url = 'mongodb://localhost/tao-book-help';
         let url = process.env.PROD_MONGODB;
         if (mode === 'test') {
             url = 'mongodb://localhost/tao-book-help-test';
